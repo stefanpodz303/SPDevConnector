f19b8388d944dded325523191a92e6f4fc906261
The Podz <stefanpodz@yahoo.com>
2021-10-14 11:41:34 -0400

    updated gitignore

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// GET api/users
-// test route
-router.get('/', (req, res) => res.send('User route'));
+// POST api/users
+// register user route
+router.post('/', (req, res) => res.send('User route'));
 
 module.exports = router;
